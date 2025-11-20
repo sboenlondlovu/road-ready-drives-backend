f94766297b2cde29ca13127391b4a6989026f03f
sboenlondlovu <sboenlondlovu@gmail.com>
2025-11-20 15:59:26 +0200

    Add files via upload
    Signed-off-by: sboenlondlovu <sboenlondlovu@gmail.com>

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-// script.js
-// Basic JavaScript for your website
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Website Loaded Successfully!");
-
-    // Example button click handler
-    const btn = document.querySelector("#actionBtn");
-    if (btn) {
-        btn.addEventListener("click", () => {
-            alert("Button clicked!");
-        });
-    }
-});
+console.log("Road Ready Website Loaded");
